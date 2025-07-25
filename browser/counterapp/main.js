7e626304c3dcec148f564aa433cb14b8a070f0ea
Sara <sarahsulokoka@gmail.com>
2025-07-25 19:23:23 +0300

    main.js updated

--- browser/counterapp/main.js
+++ browser/counterapp/main.js
@@ -1,6 +1,9 @@
 const DEFAULT = 0;
 let counter = DEFAULT //state - data 
 
+
+//Controller
+
 window.addEventListener('DOMContentLoaded', function() {
     this.window.document.querySelector("#btnDecr").addEventListener('click', () => onDecreaseClicked())
     
@@ -10,12 +13,43 @@ window.addEventListener('DOMContentLoaded', function() {
   })
 
 
+  //Model
+
  function onDecreaseClicked() {
     decreaseCounter()
 }
  function onResetClicked() {
    resetCounter()
 }
- function onIncreseClicked() {
+ function onIncreaseClicked() {
    increaseCounter()
 }
+
+function decreaseCounter() {
+    counter--;
+    render()
+}
+
+function resetCounter() {
+    counter = DEFAULT;
+    render()
+}
+
+function increaseCounter() {
+    counter++;
+    render()
+}
+
+
+function render() {
+    const counterDOM = document.querySelector('#counter')
+    counterDOM.textContent = counter
+    styleCounter(counterDOM)
+}
+
+function styleCounter(counterDOM) {
+    counterDOM.classList.toggle('color-green', counter > 0)
+    counterDOM.classList.toggle('color-red', counter < 0)
+    counterDOM.classList.toggle('color-black', counter === 0)
+}  
+
